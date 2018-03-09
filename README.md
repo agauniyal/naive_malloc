6bebda23f6919b58ec96e5b674e57af8e236b4ff
agauniyal <agauniyal@outlook.com>
2018-03-09 22:22:34 +0530

    Add initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# naive_malloc
-An implementation of naive and bare bones malloc
+```
+$ clang++ main.cpp -std=c++17 -Wall -Wextra -stdlib=libc++
+$ valgrind ./a.out
+==23493== Memcheck, a memory error detector
+==23493== Copyright (C) 2002-2017, and GNU GPL'd, by Julian Seward et al.
+==23493== Using Valgrind-3.13.0 and LibVEX; rerun with -h for copyright info
+==23493== Command: ./a.out
+==23493==
+1: 0x4227018
+2: 0x4227018
+3: 0x4227018
+4: 0x4227018
+5: 0x4227018
+==23493==
+==23493== HEAP SUMMARY:
+==23493==     in use at exit: 0 bytes in 0 blocks
+==23493==   total heap usage: 0 allocs, 0 frees, 0 bytes allocated
+==23493==
+==23493== All heap blocks were freed -- no leaks are possible
+==23493==
+==23493== For counts of detected and suppressed errors, rerun with: -v
+==23493== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+```
---
+++ main.cpp
@@ -0,0 +1,124 @@
+// clang++ main.cpp -std=c++17 -Wall -Wextra -stdlib=libc++
+
+#include <cstring>
+#include <cstdio>
+#include <optional>
+
+#include <unistd.h>
+
+void *global_base = nullptr;
+
+struct block_meta {
+    size_t size;
+    block_meta *next;
+    bool free;
+};
+
+block_meta *find_free_block(block_meta **last, const size_t size)
+{
+    block_meta *current = static_cast<block_meta *>(global_base);
+    while (current && !(current->free && current->size >= size)) {
+        *last   = current;
+        current = current->next;
+    }
+    return current;
+}
+
+
+block_meta *request_space(block_meta *last, const size_t size)
+{
+    block_meta *block = static_cast<block_meta *>(sbrk(0));
+    void *request     = sbrk(size + sizeof(block_meta));
+    if (request == (void *) -1) {
+        return nullptr;
+    }
+
+    if (last) {
+        last->next = block;
+    }
+    block->size = size;
+    block->next = nullptr;
+    block->free = false;
+    return block;
+}
+
+std::optional<void *> my_malloc(const size_t size)
+{
+    if (size == 0) {
+        return {};
+    }
+    block_meta *block;
+    if (!global_base) {
+        block = request_space(nullptr, size);
+        if (!block) {
+            return {};
+        }
+        global_base = block;
+    } else {
+        block_meta *last = static_cast<block_meta *>(global_base);
+        block            = find_free_block(&last, size);
+        if (block) {
+            block->free = false;
+        } else if (block = request_space(last, size); !block) {
+            return {};
+        }
+    }
+
+    return (block + 1);
+}
+
+void my_free(void *ptr)
+{
+    if (ptr) {
+        const auto block_ptr = static_cast<block_meta *>(ptr) - 1;
+        block_ptr->free      = true;
+    }
+}
+
+std::optional<void *> my_realloc(void *ptr, size_t size)
+{
+    if (!ptr) {
+        return my_malloc(size);
+    }
+
+    block_meta *block_ptr = static_cast<block_meta *>(ptr) - 1;
+    if (block_ptr->size >= size) {
+        return ptr;
+    }
+
+    if (auto new_ptr = my_malloc(size); *new_ptr) {
+        memcpy(*new_ptr, ptr, block_ptr->size);
+        my_free(ptr);
+        return *new_ptr;
+    } else {
+        return {};
+    }
+}
+
+std::optional<void *> my_calloc(size_t numElems, size_t elemSize)
+{
+    size_t size = numElems * elemSize;
+    if (auto result = my_malloc(size); *result) {
+        memset(*result, 0, size);
+        return *result;
+    } else {
+        return {};
+    }
+}
+
+int main()
+{
+    // prevent any allocations on clang by providing buffer to stdout
+    char buffer[256];
+    setvbuf(stdout, buffer, _IOLBF, sizeof(buffer));
+
+    for (const auto i : { 1, 2, 3, 4, 5 }) {
+        const auto p = my_malloc(8);
+        if (p) {
+            printf("%d: %p\n", i, (void *) *p);
+        } else {
+            puts("\nAllocation failed!!\n");
+        }
+        my_free(*p);
+    }
+}
